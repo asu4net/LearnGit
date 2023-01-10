40da6f2715bad6bcfc67bb8458b0a74380cfd529
Alex <asu4net@gmail.com>
2023-01-10 12:00:12 +0100

    Added book plots in main function

--- LearnGit/src/LearnGit.cpp
+++ LearnGit/src/LearnGit.cpp
@@ -5,8 +5,17 @@ int main()
     LOG("Hello World!");
     LOG("Read Brandon Sanderson!");
     LOG("You should start in Mistborn, Stormlight Archive, or Elantris");
-    LOG("Mistborn is a story that goes through ages.");
-    LOG("Stormlight Archive is a epic fantasy book");
-    LOG("Elantris is a single book fantasy story");
+    
+    LOG(" The main character in Mistborn is Vin, a 16-year-old thief on the streets of Luthadel, "
+        "the capital city of the Final Empire. The story follows her life as she comes into her own as one of the Mistborn, "
+        "people who can use ingested metal particles to enhance their abilities, both mental and physical. ");
+    
+    LOG("The backstory of the novel revolves around recurring disasters known as Desolations, "
+        "where monstrous Voidbringers ravage the world and human survival hangs in the balance. "
+        "To counter the threat, the Knights Radiant (so named for their glowing aura and eyes) possess magical armor and "
+        "swords known as Shardplate and Shardblades as well as magical powers. The last Desolation was believed to be the final one, "
+        "and has become a time of myths and legends, "
+        "in particular as the Knights Radiant left behind their weapons and armor and disappeared into obscurity. ");
+    
     return 0;
 }

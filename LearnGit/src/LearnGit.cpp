2dc420ed369a2f29d6f7cf1b6a1a7442a40116a5
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2023-01-10 11:50:52 +0100

    Added dummy haha log

--- LearnGit/src/LearnGit.cpp
+++ LearnGit/src/LearnGit.cpp
@@ -5,5 +5,6 @@ int main()
     LOG("Hello World!");
     LOG("Read Brandon Sanderson!");
     LOG("You should start in Mistborn, Stormlight Archive, or Elantris");
+    LOG("haha");
     return 0;
 }

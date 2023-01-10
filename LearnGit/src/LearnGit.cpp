47048fcb985bd4af71d8df6398775d73a5f28bda
Alex <asu4net@gmail.com>
2023-01-10 11:49:18 +0100

    Added Stormlight Archive explanation in main function

--- LearnGit/src/LearnGit.cpp
+++ LearnGit/src/LearnGit.cpp
@@ -6,5 +6,6 @@ int main()
     LOG("Read Brandon Sanderson!");
     LOG("You should start in Mistborn, Stormlight Archive, or Elantris");
     LOG("Mistborn is a story that goes through ages.");
+    LOG("Stormlight Archive is a epic fantasy book");
     return 0;
 }
